bf8ec2e9cf0d5bfba0622ac8e12907cdd7934a50
Luis Martinez de Bartolome <lasote@gmail.com>
2017-08-08 17:22:39 +0200

    Not 3.0.2 travis

--- build.py
+++ build.py
@@ -1,3 +1,5 @@
+import platform
+
 from conan.packager import ConanMultiPackager
 
 available_versions = ["3.0.2", "3.9.0", "3.8.2", "3.7.2", "3.6.3",
@@ -7,5 +9,7 @@ available_versions = ["3.0.2", "3.9.0", "3.8.2", "3.7.2", "3.6.3",
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     for version in available_versions:
-       builder.add({}, {"cmake_installer:version": version}, {}, {})
+        # Unknown problem with 3.0.2 on travis
+        if version != "3.0.2" or platform.system() != "Linux":
+            builder.add({}, {"cmake_installer:version": version}, {}, {})
     builder.run()
