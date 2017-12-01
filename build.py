262ac61f0a570395b9fc986ce1a20389cc8cfdb6
Konrad Adasiewicz <konrad.adasiewicz@comarch.com>
2017-12-01 09:40:43 +0100

    added cmake version 3.10.0, improved minor version part extraction, automated default version setting to newest one

--- build.py
+++ build.py
@@ -2,9 +2,11 @@ import platform
 
 from conan.packager import ConanMultiPackager
 
-available_versions = ["3.9.0", "3.8.2", "3.7.2", "3.6.3",
-                      "3.5.2", "3.4.3", "3.3.2", "3.2.3", "3.1.3", "3.0.2",
-                      "2.8.12"]
+available_versions = ["3.10.0", "3.9.0", "3.8.2",
+                      "3.8.1", "3.8.0", "3.7.2", "3.7.1",
+                      "3.7.0", "3.6.3", "3.6.2", "3.6.1",
+                      "3.6.0", "3.5.2", "3.4.3", "3.3.2",
+                      "3.2.3", "3.1.3", "3.0.2", "2.8.12"]
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
