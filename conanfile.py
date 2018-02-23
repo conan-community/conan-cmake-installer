6dfac21674496001af457923d69dcbf23e8344cc
Werner Palfinger <werner.palfinger@profactor.at>
2018-02-23 09:33:10 +0100

    added CMake 3.11.0-rc1, keeping 3.10 as default

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import tools, ConanFile
 from conans import __version__ as conan_version
 from conans.model.version import Version
 
-available_versions = ["3.10.0", "3.9.0", "3.8.2",
+available_versions = ["3.11.0-rc1", "3.10.0", "3.9.0", "3.8.2",
                       "3.8.1", "3.8.0", "3.7.2", "3.7.1",
                       "3.7.0", "3.6.3", "3.6.2", "3.6.1",
                       "3.6.0", "3.5.2", "3.4.3", "3.3.2",
@@ -21,7 +21,7 @@ class CMakeInstallerConan(ConanFile):
     else:
         settings = "os_build", "arch_build"
     options = {"version": available_versions}
-    default_options = "version=" + available_versions[0]
+    default_options = "version=3.10.0"
     build_policy = "missing"
 
     def minor_version(self):
