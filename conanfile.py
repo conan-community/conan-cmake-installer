f95165b544cdb8f4f089670ce04de805a78a81f6
Torfinn Berset <torfinn@bloom-life.com>
2019-10-02 16:59:56 +0200

    Add CMake 3.15.4

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import __version__ as conan_version
 from conans.model.version import Version
 from conans.errors import ConanInvalidConfiguration, NotFoundException, ConanException
 
-available_versions = ["3.15.3", "3.15.2", "3.15.1", "3.15.0",
+available_versions = ["3.15.4", "3.15.3", "3.15.2", "3.15.1", "3.15.0",
                       "3.14.6", "3.14.5", "3.14.4", "3.14.3", "3.14.2", "3.14.1", "3.14.0",
                       "3.13.4", "3.13.3", "3.13.2", "3.13.1", "3.13.0",
                       "3.12.4", "3.12.3", "3.12.2", "3.12.1", "3.12.0",
